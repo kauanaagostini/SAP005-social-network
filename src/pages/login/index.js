d7eac38eea05918e04f3696ceec330921bd5cda6
Kauana Maria Agostini <kauanaagostini@gmail.com>
2020-12-16 16:53:34 -0300

    criado pagina login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,12 +2,19 @@ export const Login = () => {
   // Coloque sua página
   const rootElement = document.createElement('div');
   rootElement.innerHTML = `
-      <form id=>
+      <form id=formLogin>
         <h2>Acesse sua conta</h2>
-        <label class="title" for="User" >Usuário</label>
-        <input class="input" type="e-mail" placeholder="Informe e-mail cadastrado" required/>
-        <label class="title" for="password">Senha</label>
-        <input class="input" type="password" placeholder="Informe sua senha" required/>
+        <p>
+          <label class="title" for="User" >Usuário</label>
+          <input class="input" type="e-mail" placeholder="Informe e-mail cadastrado" required>
+        </p>
+        <p>
+          <label class="title" for="password">Senha  </label>
+          <input class="input" type="password" placeholder="Informe sua senha" required>
+        </p>
+        <button>Entrar</button>
+        <script src="https://www.gstatic.com/firebasejs/ui/4.7.1/firebase-ui-auth.js"></script>
+        <link type="text/css" rel="stylesheet" href="https://www.gstatic.com/firebasejs/ui/4.7.1/firebase-ui-auth.css" />
       </form>
   `;
   return rootElement;
