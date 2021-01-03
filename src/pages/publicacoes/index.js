f401081384f6a87a79d750d5068cc4096b9db214
alencartha <alencar_tha@hotmail.com>
2021-01-02 21:30:27 -0300

    posts ordenados por mais recentes, icones de like e delete integrando com firestore

--- src/pages/publicacoes/index.js
+++ src/pages/publicacoes/index.js
@@ -1,4 +1,4 @@
-import { getPosts, handleSignOut} from "../../services/index.js";
+import { getPosts, handleSignOut, likePost, deletePost} from "../../services/index.js";
 import { onNavigate } from "../../utils/history.js";
 
 
@@ -8,7 +8,7 @@ export const publicacoes = () => {
     const addPost = (post) => {
         const postTemplate =
             `
-            <section id="post-container" id="${post.id}">
+            <section class="post-container" data-id=${post.id}>
                 <div class="post-item">
                     <img src="../../img/user.png">
                     <p>${post.data().name}</p>
@@ -22,24 +22,34 @@ export const publicacoes = () => {
             </section>
             <section id="container-edit"> 
               <div class="item-edit">
-                <img src="../../img/like.png" alt="like" id="like">
-                <p id="number-of-likes">${post.data().likes} </p>
+                <img src="../../img/like.png" alt="like" class="btn-like" data-id=${post.id}>
+                <p id="number-of-likes" data-id=${post.id}>${post.data().likes} </p>
               </div>
               <div class="item-edit">
-                  <p id="edit-post">EDITAR</p>
-                  <p id="delete-post">DELETAR</p>
+                  <p id="edit-post"data-id=${post.id}>EDITAR</p>
+                  <p id="delete-post" data-id=${post.id}>DELETAR</p>
               </div>     
             </section>
         `
-
         document.getElementById("text").innerHTML += postTemplate;
-        document.getElementById("hello-user").innerHTML = `Olá, ${firebase.auth().currentUser.displayName} !`;
-    };
 
+        const btnLike = document.querySelector(".btn-like")
+        btnLike.addEventListener("click", ()=>{
+            likePost(btnLike.dataset.id)
+            console.log(btnLike.dataset.id)
+        })
+
+        const btnDelete = document.querySelector("#delete-post")
+        btnDelete.addEventListener("click", (event)=>{
+            event.preventDefault();
+            deletePost(btnDelete.dataset.id)
+        })
+    }
+    
+   
     getPosts().then(snap => {
-        snap.forEach(doc => {
-            addPost(doc)
-            //console.log(doc.data())
+        snap.forEach(post => {
+            addPost(post)
         });
     })
 
@@ -57,7 +67,7 @@ export const publicacoes = () => {
     <main>
         <section id="user-container">
             <img src="../../img/user.png" alt="Logo do Site" class="user-item">
-            <h2 class="user-item" id="hello-user"> </h2>
+            <h2 class="user-item" id="hello-user">Olá, ${firebase.auth().currentUser.displayName}!</h2>
         </section>
         <section id="option-container">
             <h3 ><a href="publicar" class="option-item" id="posts-view">Publicar</a></h3>
@@ -78,9 +88,9 @@ export const publicacoes = () => {
         handleSignOut()
         onNavigate("/login")
     })
-
-
-
+  
     return rootElement;
 };
+
+
 
