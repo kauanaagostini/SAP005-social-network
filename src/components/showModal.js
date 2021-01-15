58bf61aa9475511caca2a97518410e682e0b19e6
Alessandra Alves <a.ale_alves@hotmail.com>
2021-01-14 22:11:17 -0300

    teste publicar

--- src/components/showModal.js
+++ src/components/showModal.js
@@ -1,21 +1,20 @@
 export const showModal = {
-    error(message) {
-        let errorTemplate = document.querySelector('#errorModal');
-        let errorMessage = document.createElement('section');
-        errorMessage.setAttribute('id', "errorMessage")
-        errorTemplate.classList.add('itemActive');
-        errorTemplate.appendChild(errorMessage)
-        errorMessage.innerHTML =  `
+  error(message) {
+    const errorTemplate = document.querySelector('#errorModal');
+    const errorMessage = document.createElement('section');
+    errorMessage.setAttribute('id', 'errorMessage');
+    errorTemplate.classList.add('itemActive');
+    errorTemplate.appendChild(errorMessage);
+    errorMessage.innerHTML = `
             <h1> Atenção! </h1>
             <p> ${message} </p>
             <button type='button' class='btnClose'> Ok </button>
-        `
-        
-        const btnClose = errorTemplate.querySelector('.btnClose')
-        btnClose.addEventListener('click', () => {
-            errorTemplate.classList.remove('itemActive')
-            errorMessage.innerHTML= "";
-        })
-    }
-}
+        `;
 
+    const btnClose = errorTemplate.querySelector('.btnClose');
+    btnClose.addEventListener('click', () => {
+      errorTemplate.classList.remove('itemActive');
+      errorMessage.innerHTML = '';
+    });
+  },
+};
--- src/services/index.js
+++ src/services/index.js
@@ -11,7 +11,7 @@ export const handleSignUp = (email, password, firstName, lastName) => {
     .then((user) => {
       const userName = `${firstName} ${lastName}`;
       user.user.updateProfile({ displayName: userName });
-      const message = "Usuário criado com sucesso!"
+      const message = 'Usuário criado com sucesso!';
       showModal.error(message);
       onNavigate('/publicacoes');
     })
@@ -53,7 +53,7 @@ export const handleSignOut = () => {
 
 export const validatePassword = (password, samePassword) => {
   if (password !== samePassword) {
-    const errorMessage = "As senhas digitadas são divergentes"
+    const errorMessage = 'As senhas digitadas são divergentes';
     showModal.error(errorMessage);
     return false;
   }
@@ -62,50 +62,32 @@ export const validatePassword = (password, samePassword) => {
 
 export const validateEmptyInput = (firstName, lastName) => {
   if (firstName.length < 1 || lastName.length < 1) {
-    const errorMessage = "Os campos Nome e Sobrenome são de preenchimentos obrigatórios"
+    const errorMessage = 'Os campos Nome e Sobrenome são de preenchimentos obrigatórios';
     showModal.error(errorMessage);
     return false;
   }
   return true;
 };
 
-
-// ----- POSTS -----
-
-export const postImage = (photo, callback) => {
-  const file = photo.files[0];
-  const storageRef = firebase.storage().ref(`imagens/${file.name}`);
-
-  storageRef.put(file).then(() => {
-    storageRef.getDownloadURL().then((url) => {
-      callback(url);
-    });
-  });
-};
-
 export const createPost = (post) => {
-    const user = firebase.auth().currentUser;
-    const date = new Date();
-    firebase
-      .firestore()
-      .collection("post")
-      .add({
-        name: user.displayName,
-        user_id: user.uid,
-        text: post,
-        date: date.toLocaleString(),
-        time: date.getTime(),
-        usersLike: [],
-        usersDislike: [],
+  const user = firebase.auth().currentUser;
+  const date = new Date();
+  firebase
+    .firestore()
+    .collection('post')
+    .add({
+      name: user.displayName,
+      user_id: user.uid,
+      text: post,
+      date: date.toLocaleString(),
+      time: date.getTime(),
+      usersLike: [],
+      usersDislike: [],
 
-      })
-      .then(() => {
-        return Promise.resolve(true);
-      })
-      .catch((error) => {
-        return Promise.reject(error);
-      });
-  };
+    })
+    .then(() => Promise.resolve(true))
+    .catch((error) => Promise.reject(error));
+};
 
 export const getPosts = () => {
   const post = firebase
@@ -120,35 +102,32 @@ export const likePost = (id, userID) => {
   const postLike = firebase.firestore().collection('post').doc(id);
   return postLike.update({
     usersLike: userLike,
-  })
- 
-}
+  });
+};
 
-export const removeLike =(id, userId) =>{
+export const removeLike = (id, userId) => {
   const userLike = firebase.firestore.FieldValue.arrayRemove(userId);
-  const postLike = firebase.firestore().collection("post").doc(id);
+  const postLike = firebase.firestore().collection('post').doc(id);
   return postLike.update({
     usersLike: userLike,
-  })
-
-}
+  });
+};
 
 export const dislikePost = (id, userID) => {
   const userDislike = firebase.firestore.FieldValue.arrayUnion(userID);
-  const postDislike = firebase.firestore().collection("post").doc(id);
-   return postDislike.update({
+  const postDislike = firebase.firestore().collection('post').doc(id);
+  return postDislike.update({
     usersDislike: userDislike,
-  })
-}
+  });
+};
 
 export const removeDislike = (id, userId) => {
   const userDislike = firebase.firestore.FieldValue.arrayRemove(userId);
   const postDislike = firebase.firestore().collection('post').doc(id);
   return postDislike.update({
     usersDislike: userDislike,
-  })
-
-}
+  });
+};
 
 export const editPost = (text, id) => {
   const postUpdate = firebase.firestore().collection('post').doc(id);
